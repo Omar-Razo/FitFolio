cd646e5b8e930880b5844b89d431ee53db59153c
Nicolas Esquibel <n.m.esquibel01@gmail.com>
2023-09-20 20:34:07 -0700

    added login function and logout function

--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,42 @@
-const withAuth = (req, res, next) => {
-  if (!req.session.logged_in) {
-    res.redirect('/login');
-  } else {
-    next();
-  }
-};
+import { useHistory } from 'react-router-dom';
 
-module.exports = withAuth;
+const withAuth = (Component) => {
+  const AuthComponent = () => {
+    const history = useHistory();
+    const userRole = sessionStorage.getItem('role');
+    
+    if (!sessionStorage.getItem('logged_in')) {
+      console.error('User not logged in');
+      history.push('/login');
+      return null;
+    }
+    
+    // Redirect to a specific page after login
+    if (sessionStorage.getItem('logged_in')) {
+      history.push('/dashboard');
+      return null;
+    }
+    
+    const handleLogout = () => {
+      // Clear session storage and redirect to login page
+      sessionStorage.removeItem('logged_in');
+      history.push('/login');
+    }
+    
+    // Check user role for access control
+    if (userRole !== 'admin') {
+      console.error('User does not have permission to access this page');
+      history.push('/unauthorized');
+      return null;
+    }
+    
+    return (
+      <>
+        <button onClick={handleLogout}>Logout</button>
+        <Component />
+      </>
+    );
+  }
+  
+  return AuthComponent;
+}
