9b1bd8cdeb2e58f6f24129f1cd70e148ef8f090a
Nicolas Esquibel <n.m.esquibel01@gmail.com>
2023-09-21 19:46:35 -0700

    updates

--- utils/auth.js
+++ utils/auth.js
@@ -1,42 +1,56 @@
-import { useHistory } from 'react-router-dom';
-
 const withAuth = (Component) => {
   const AuthComponent = () => {
     const history = useHistory();
+    const isLoggedIn = sessionStorage.getItem('logged_in');
     const userRole = sessionStorage.getItem('role');
-    
-    if (!sessionStorage.getItem('logged_in')) {
+
+    const handleLogout = () => {
+      sessionStorage.removeItem('logged_in');
+      history.push('/login');
+    };
+
+    // Error handling
+    if (!isLoggedIn) {
       console.error('User not logged in');
       history.push('/login');
       return null;
     }
-    
-    // Redirect to a specific page after login
-    if (sessionStorage.getItem('logged_in')) {
-      history.push('/dashboard');
-      return null;
-    }
-    
-    const handleLogout = () => {
-      // Clear session storage and redirect to login page
-      sessionStorage.removeItem('logged_in');
-      history.push('/login');
-    }
-    
-    // Check user role for access control
-    if (userRole !== 'admin') {
+
+    // Role-based access control
+    const allowedRoles = ['admin']; // Modify this array with allowed roles
+    if (!allowedRoles.includes(userRole)) {
       console.error('User does not have permission to access this page');
       history.push('/unauthorized');
       return null;
     }
-    
+
+    // Redirecting to a specific page after login
+    const intendedDestination = sessionStorage.getItem('intended_destination');
+    if (intendedDestination) {
+      sessionStorage.removeItem('intended_destination');
+      history.push(intendedDestination);
+      return null;
+    }
+
+    // Authentication timeout
+    const timeoutDuration = 30 * 60 * 1000; // 30 minutes
+    const lastActivityTime = sessionStorage.getItem('last_activity_time');
+    const currentTime = new Date().getTime();
+    if (lastActivityTime && currentTime - lastActivityTime > timeoutDuration) {
+      console.error('Session timeout');
+      sessionStorage.removeItem('logged_in');
+      history.push('/login');
+      return null;
+    }
+    sessionStorage.setItem('last_activity_time', currentTime);
+
     return (
       <>
         <button onClick={handleLogout}>Logout</button>
         <Component />
       </>
     );
-  }
-  
+  };
+
   return AuthComponent;
-}
+};
