275a16b955108d8cd4876f1859293f37f1deff3b
Nicolas Esquibel <n.m.esquibel01@gmail.com>
2023-09-25 21:02:40 -0700

    updated loginandlogout functions with middleware

--- utils/auth.js
+++ utils/auth.js
@@ -1,56 +1,44 @@
-const withAuth = (Component) => {
-  const AuthComponent = () => {
-    const history = useHistory();
-    const isLoggedIn = sessionStorage.getItem('logged_in');
-    const userRole = sessionStorage.getItem('role');
+const withAuth = (req, res, next) => {
+  const isLoggedIn = req.session.logged_in;
+  const userRole = req.session.role;
 
-    const handleLogout = () => {
-      sessionStorage.removeItem('logged_in');
-      history.push('/login');
-    };
-
-    // Error handling
-    if (!isLoggedIn) {
-      console.error('User not logged in');
-      history.push('/login');
-      return null;
-    }
-
-    // Role-based access control
-    const allowedRoles = ['admin']; // Modify this array with allowed roles
-    if (!allowedRoles.includes(userRole)) {
-      console.error('User does not have permission to access this page');
-      history.push('/unauthorized');
-      return null;
-    }
-
-    // Redirecting to a specific page after login
-    const intendedDestination = sessionStorage.getItem('intended_destination');
-    if (intendedDestination) {
-      sessionStorage.removeItem('intended_destination');
-      history.push(intendedDestination);
-      return null;
-    }
-
-    // Authentication timeout
-    const timeoutDuration = 30 * 60 * 1000; // 30 minutes
-    const lastActivityTime = sessionStorage.getItem('last_activity_time');
-    const currentTime = new Date().getTime();
-    if (lastActivityTime && currentTime - lastActivityTime > timeoutDuration) {
-      console.error('Session timeout');
-      sessionStorage.removeItem('logged_in');
-      history.push('/login');
-      return null;
-    }
-    sessionStorage.setItem('last_activity_time', currentTime);
-
-    return (
-      <>
-        <button onClick={handleLogout}>Logout</button>
-        <Component />
-      </>
-    );
+  const handleLogout = () => {
+    req.session.logged_in = false;
+    res.redirect('/login');
   };
 
-  return AuthComponent;
+  if (!isLoggedIn) {
+    console.error('User not logged in');
+    res.redirect('/login');
+    return;
+  }
+
+  const allowedRoles = ['admin'];
+  if (!allowedRoles.includes(userRole)) {
+    console.error('User does not have permission to access this page');
+    res.redirect('/unauthorized');
+    return;
+  }
+
+  const intendedDestination = req.session.intended_destination;
+  if (intendedDestination) {
+    delete req.session.intended_destination;
+    res.redirect(intendedDestination);
+    return;
+  }
+
+  const timeoutDuration = 30 * 60 * 1000;
+  const lastActivityTime = req.session.last_activity_time;
+  const currentTime = new Date().getTime();
+  if (lastActivityTime && currentTime - lastActivityTime > timeoutDuration) {
+    console.error('Session timeout');
+    req.session.logged_in = false;
+    res.redirect('/login');
+    return;
+  }
+  req.session.last_activity_time = currentTime;
+
+  next();
 };
+
+module.exports = withAuth;
